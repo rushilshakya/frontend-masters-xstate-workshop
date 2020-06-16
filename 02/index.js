221a67743bdad042dd09e6860ffa08677fc72bb8
rushilshakya <rushil.shakya@gmail.com>
2020-06-16 22:55:04 +0545

    feat: 2 done

--- 02/index.js
+++ 02/index.js
@@ -1,20 +1,41 @@
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
   // Add your object machine definition here
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        CLICK: "active",
+      },
+    },
+    active: {
+      on: {
+        CLICK: "inactive",
+      },
+    },
+  },
 });
 
 // Change this to the initial state
-let currentState = undefined;
+let currentState = machine.initialState;
+
+const thisService = interpret(machine);
+
+thisService.onTransition(state => console.log(state.value));
+
+thisService.start();
 
 function send(event) {
   // Determine and update the `currentState`
+  // currentState = machine.transition(currentState, event);
 
-  elBox.dataset.state = currentState.value;
+  elBox.dataset.state = thisService.send(event);
 }
 
-elBox.addEventListener('click', () => {
+elBox.addEventListener("click", () => {
   // Send a click event
+  send("CLICK");
 });
