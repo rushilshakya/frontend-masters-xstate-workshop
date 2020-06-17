b25392a07cbae0ec9b354247b34de1e6731755d2
rushilshakya <rushil.shakya@gmail.com>
2020-06-17 21:04:26 +0545

    feat: lesson 3 complete

--- 03/index.js
+++ 03/index.js
@@ -1,28 +1,48 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
   // Create your state machine here
+  initial: "inactive",
+  states: {
+    active: {
+      on: {
+        mouseup: "inactive",
+      },
+    },
+    inactive: {
+      on: {
+        mousedown: "active",
+      },
+    },
+  },
   // ...
 });
 
 // Create a service using interpret(...)
-const service = undefined;
+const service = interpret(machine);
 
 // Listen to state transitions and set
 // `elBox.dataset.state` to the state value as before.
 // ...
+service.onTransition(state => (elBox.dataset.state = state.value));
 
 // Start the service.
 // ...
+service.start();
+window.setTimeout(service.stop, 5000);
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", event => {
   // Send a mousedown event
+  service.send(event);
+
   // ...
 });
 
-elBox.addEventListener('mouseup', (event) => {
+elBox.addEventListener("mouseup", event => {
   // Send a mouseup event
+  service.send(event);
+
   // ...
 });
