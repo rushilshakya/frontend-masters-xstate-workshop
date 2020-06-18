a7d2112434d748edfe11cdcce67f999cade70b3c
rushilshakya <rushil.shakya@gmail.com>
2020-06-18 10:18:53 +0545

    feat: exercise 5 complete

--- 04/index.js
+++ 04/index.js
@@ -1,37 +1,84 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret, assign } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const setPoint = (context, event) => {
-  // Set the data-point attribute of `elBox`
-  // ...
+  elBox.dataset.point = `(${event.clientX}, ${event.clientY})`;
 };
 
-const machine = createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Add your action here
-          // ...
-          target: 'dragging',
+const enterDrag = (context, event) => {
+  console.log("enter drag, count is ", context.count);
+};
+
+const dragMouseUp = (context, event) => {
+  console.log("drag mouseUp");
+};
+
+const countUp = assign({
+  count: (context, event) => context.count + 1,
+});
+
+console.log(
+  assign({
+    name: "rushil",
+    count: context => context.count + 1,
+  })
+);
+// const countUp = assign((context, value) => {
+//   return {
+//     count: context.count + 1,
+//   };
+// });
+
+const exitDrag = (context, event) => {
+  console.log("after drag, the count is ", context.count);
+};
+
+const machine = createMachine(
+  {
+    initial: "idle",
+    context: {
+      count: 0,
+    },
+    states: {
+      idle: {
+        on: {
+          mousedown: {
+            // Add your action here
+            // ...
+            actions: [setPoint],
+            target: "dragging",
+          },
         },
       },
-    },
-    dragging: {
-      on: {
-        mouseup: {
-          target: 'idle',
+      dragging: {
+        entry: enterDrag,
+        on: {
+          mouseup: {
+            target: "idle",
+            actions: dragMouseUp,
+          },
         },
+        exit: [exitDrag, countUp],
+        // exit: countUp,
+        // exit: assign({
+        //   count: (context, event) => context.count + 1,
+        // }),
       },
     },
   },
-});
+  {
+    actions: {
+      setPoints: () => {
+        // console.log("overriding!");
+      },
+    },
+  }
+);
 
 const service = interpret(machine);
 
-service.onTransition((state) => {
+service.onTransition(state => {
   console.log(state);
 
   elBox.dataset.state = state.value;
@@ -39,10 +86,10 @@ service.onTransition((state) => {
 
 service.start();
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", event => {
   service.send(event);
 });
 
-elBox.addEventListener('mouseup', (event) => {
+elBox.addEventListener("mouseup", event => {
   service.send(event);
 });
--- 05/index.js
+++ 05/index.js
@@ -1,40 +1,52 @@
-import { createMachine, assign, interpret } from 'xstate';
+import { createMachine, assign, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 const elBody = document.body;
 
 const machine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   // Set the initial context
-  // Clue: {
-  //   x: 0,
-  //   y: 0,
-  //   dx: 0,
-  //   dy: 0,
-  //   px: 0,
-  //   py: 0,
-  // }
-  // context: ...,
+  context: {
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0,
+    px: 0,
+    py: 0,
+  },
   states: {
     idle: {
       on: {
         mousedown: {
           // Assign the point
           // ...
-          target: 'dragging',
+          target: "dragging",
+          actions: assign({
+            px: (context, event) => event.clientX,
+            py: (context, event) => event.clientY,
+          }),
         },
       },
     },
     dragging: {
       on: {
         mousemove: {
-          // Assign the delta
-          // ...
-          // (no target!)
+          actions: assign({
+            dx: (context, event) => event.clientX - context.px,
+            dy: (context, event) => event.clientY - context.py,
+          }),
         },
         mouseup: {
           // Assign the position
-          target: 'idle',
+          actions: assign({
+            dx: 0,
+            dy: 0,
+            px: 0,
+            py: 0,
+            x: context => context.x + context.dx,
+            y: context => context.y + context.dy,
+          }),
+          target: "idle",
         },
       },
     },
@@ -43,16 +55,16 @@ const machine = createMachine({
 
 const service = interpret(machine);
 
-service.onTransition((state) => {
+service.onTransition(state => {
   if (state.changed) {
     console.log(state.context);
 
     elBox.dataset.state = state.value;
 
-    elBox.style.setProperty('--dx', state.context.dx);
-    elBox.style.setProperty('--dy', state.context.dy);
-    elBox.style.setProperty('--x', state.context.x);
-    elBox.style.setProperty('--y', state.context.y);
+    elBox.style.setProperty("--dx", state.context.dx);
+    elBox.style.setProperty("--dy", state.context.dy);
+    elBox.style.setProperty("--x", state.context.x);
+    elBox.style.setProperty("--y", state.context.y);
   }
 });
 
@@ -60,5 +72,8 @@ service.start();
 
 // Add event listeners for:
 // - mousedown on elBox
+elBox.addEventListener("mousedown", service.send);
 // - mousemove on elBody
+elBody.addEventListener("mousemove", service.send);
 // - mouseup on elBody
+elBody.addEventListener("mouseup", service.send);
