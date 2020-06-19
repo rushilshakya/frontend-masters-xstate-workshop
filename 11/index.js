368d4321aaf2609abcc8860bcd5548d08bdae285
rushilshakya <rushil.shakya@gmail.com>
2020-06-19 18:41:39 +0545

    feat: 11 complete

--- 11/index.js
+++ 11/index.js
@@ -1,22 +1,65 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elApp = document.querySelector('#app');
-const elOffButton = document.querySelector('#offButton');
-const elOnButton = document.querySelector('#onButton');
-const elModeButton = document.querySelector('#modeButton');
+const elApp = document.querySelector("#app");
+const elOffButton = document.querySelector("#offButton");
+const elOnButton = document.querySelector("#onButton");
+const elModeButton = document.querySelector("#modeButton");
 
 const displayMachine = createMachine({
-  initial: 'hidden',
+  initial: "hidden",
   states: {
     hidden: {
       on: {
-        TURN_ON: 'visible',
+        TURN_ON: "visible.mode.hist",
       },
     },
     visible: {
       // Add parallel states here for:
+      type: "parallel",
+      on: {
+        TURN_OFF: "hidden",
+      },
       // - mode (light or dark)
-      // - brightness (bright or dim)
+      states: {
+        mode: {
+          initial: "light",
+          states: {
+            light: {
+              on: {
+                SWITCH: "dark",
+              },
+            },
+            dark: {
+              on: {
+                SWITCH: "light",
+              },
+            },
+            hist: {
+              type: "history",
+            },
+          },
+        },
+        // - brightness (bright or dim)
+        brightness: {
+          initial: "bright",
+          states: {
+            bright: {
+              after: {
+                2000: "dim",
+              },
+            },
+            dim: {
+              on: {
+                SWITCH: "bright",
+              },
+            },
+          },
+        },
+        hist: {
+          type: "history",
+          history: "deep",
+        },
+      },
       // See the README for how the child states of each of those
       // parallel states should transition between each other.
     },
@@ -24,19 +67,19 @@ const displayMachine = createMachine({
 });
 
 const displayService = interpret(displayMachine)
-  .onTransition((state) => {
-    elApp.dataset.state = state.toStrings().join(' ');
+  .onTransition(state => {
+    elApp.dataset.state = state.toStrings().join(" ");
   })
   .start();
 
-elOnButton.addEventListener('click', () => {
-  displayService.send('TURN_ON');
+elOnButton.addEventListener("click", () => {
+  displayService.send("TURN_ON");
 });
 
-elOffButton.addEventListener('click', () => {
-  displayService.send('TURN_OFF');
+elOffButton.addEventListener("click", () => {
+  displayService.send("TURN_OFF");
 });
 
-elModeButton.addEventListener('click', () => {
-  displayService.send('SWITCH');
+elModeButton.addEventListener("click", () => {
+  displayService.send("SWITCH");
 });
